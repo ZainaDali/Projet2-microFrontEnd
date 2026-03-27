0c4ff64f424095b35edf16ed4a56c41243fef332
imana173 <imana173@gmail.com>
2026-03-27 11:15:30 +0100

    feat: cart eventBus and logic

--- mfe-cart/webpack.config.js
+++ mfe-cart/webpack.config.js
@@ -5,17 +5,20 @@ const { ModuleFederationPlugin } = require('webpack').container;
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
+
   devServer: {
     port: 3002,
     historyApiFallback: true,
     headers: { 'Access-Control-Allow-Origin': '*' },
   },
+
   resolve: {
     extensions: ['.js', '.jsx'],
     alias: {
       'shared': path.resolve(__dirname, '../shared'),
     },
   },
+
   module: {
     rules: [
       {
@@ -34,10 +37,23 @@ module.exports = {
       },
     ],
   },
+
   plugins: [
     new ModuleFederationPlugin({
-      // TODO: configurer ce MFE pour exposer le composant Cart
+      name: 'cart',
+
+      filename: 'remoteEntry.js',
+
+      exposes: {
+        './Cart': './src/components/Cart.jsx',
+      },
+
+      shared: {
+        react: { singleton: true },
+        'react-dom': { singleton: true },
+      },
     }),
+
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
