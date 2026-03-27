dbcc9114c1b9890964608bc02101260956c9c2ac
ZainaDali <dalizaina19092000@gmail.com>
2026-03-27 11:15:03 +0100

    feat(mfe-product): émettre product:added au clic Ajouter

--- mfe-product/src/components/ProductGrid.jsx
+++ mfe-product/src/components/ProductGrid.jsx
@@ -20,7 +20,10 @@ function ProductCard({ product, onAdd }) {
 
 function ProductGrid() {
   const handleAdd = (product) => {
-    // TODO: notifier le reste de l'application qu'un produit a ete ajoute
+    eventBus.emit('product:added', {
+      ...product,
+      cartId: `${Date.now()}-${Math.random()}`,
+    });
   };
 
   return (
