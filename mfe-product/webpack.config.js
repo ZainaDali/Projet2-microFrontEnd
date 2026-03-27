e883abf725d798f97f94e005e3053da9f385534d
ZainaDali <dalizaina19092000@gmail.com>
2026-03-27 10:18:18 +0100

    feat(mfe-product): configurer Module Federation port 3001

--- mfe-product/webpack.config.js
+++ mfe-product/webpack.config.js
@@ -36,8 +36,16 @@ module.exports = {
   },
   plugins: [
     new ModuleFederationPlugin({
-      // TODO: configurer ce MFE pour exposer le composant ProductGrid
-    }),
+    name: 'mfeProduct',
+    filename: 'remoteEntry.js',
+    exposes: {
+      './ProductGrid': './src/components/ProductGrid',
+    },
+    shared: {
+      react:       { singleton: true, requiredVersion: '^18.2.0' },
+      'react-dom': { singleton: true, requiredVersion: '^18.2.0' },
+    },
+  }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
